cfbf69be66809e90c22fd74cf8c34a6dae5b16a0
Ingrid Guren <ingridguren@gmail.com>
2016-10-17 16:09:06 +0200

    Bug fix compare-script

--- result/compare_results.py
+++ result/compare_results.py
@@ -1,25 +1,21 @@
 import sys
 
 try: 
-	predicted_filename = sys.argv[1]
+  predicted_filename = sys.argv[1]
 except: 
-	print "Give filename for predicted results. \nEx: python compare_results.py predicted.csv"
-	sys.exit(0)
+  print "Give filename for predicted results. \nEx: python compare_results.py predicted.csv"
+  sys.exit(0)
 
 
 antall_like = 0; 
 antall_ulike = 0; 
 
 with open('true-results.csv') as resultfile, open(predicted_filename) as predictedfile: 
-
-	for true_result, predicted in zip(resultfile, predictedfile): 
-
-
-		predicted.split(",")
-
-   		if true_result.strip() == predicted.strip()[0]: 
-   			antall_like+=1
-    	else:
-    		antall_ulike+=1
+  for true_result, predicted in zip(resultfile, predictedfile): 
+    predicted.split(",")
+    if true_result.strip() == predicted.strip()[0]: 
+      antall_like+=1
+    else:
+      antall_ulike+=1
 
 print ("Antall korrekte: %i\nAntall feil: %i" %(antall_like, antall_ulike))
